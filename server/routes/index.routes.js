85763d34f550bd71d88799b0f2c26bf0c000ffd5
Valeriya190396 <valerinakom@mail.ru>
2024-07-12 20:30:02 +0300

    feature/createSneakerRoutes

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,12 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
+const tokensRouter = require("./api/tokens.routes");
+const authRouter = require("./api/auth.routes");
+const sneakersRouter = require("./api/sneakers.routes");
+const imagesRoutes = require("./api/image.routes");
 
-const tokensRouter = require('./api/tokens.routes');
-const authRouter = require('./api/auth.routes');
-const sneakersRouter = require()
-
-
-router.use('/tokens', tokensRouter)
-router.use('/auth', authRouter)
+router.use("/tokens", tokensRouter);
+router.use("/auth", authRouter);
+router.use("/sneakers", sneakersRouter);
+router.use("/images", imagesRoutes);
 
 module.exports = router;
