11cb3c008a7b6816471cf8e65f54eb96f8c2427b
AlexanderGrebenyuk <suppose.ca@gmail.com>
2024-07-13 11:30:38 +0300

    feature/createAuthorization

---
+++ client/src/pages/authPages/AuthorizationPage.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useAppDispatch } from '../../app/store/store';
+import { authorizationThunk } from '../../entities/users/authSlice';
+import { object, string } from 'yup';
+import { UserWithoutName } from '../../entities/users/types/userTypes';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema = object().shape({
+  email: string().email().nullable().trim().required('Не email'),
+  password: string()
+    .trim()
+    .required('Необходимо указать пароль')
+    .min(5, 'пароль жолжен быть не менее 5 символов ')
+    .max(20, 'пароль должен быть не более 20 символов'),
+});
+
+function AuthorizationPage(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const onHadleSubmit = async (user: UserWithoutName): Promise<void> => {
+    void dispatch(authorizationThunk(user));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onHadleSubmit)}>
+      <label htmlFor="email">
+        Email:
+        <input type="email" {...register('email')} />
+        <span>{errors.email?.message}</span>
+      </label>
+      <br />
+      <label htmlFor="password">
+        Password:
+        <input type="password" {...register('password')} />
+        <span>{errors.password?.message}</span>
+      </label>
+      <button type="submit">login</button>
+    </form>
+  );
+}
+
+export default AuthorizationPage;
